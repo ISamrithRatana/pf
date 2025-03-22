e92ce07a78cc6f1a58b5d13e9cee24f1b625b6ab
ISamrithRatana <samrithratana@outlook.com>
2025-03-22 15:10:34 +0700

    add tailwind

--- api/index.py
+++ api/index.py
@@ -1,1 +1,11 @@
-from src.index import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Vercel!"
+
+# Vercel handler
+def handler(request):
+    return app
